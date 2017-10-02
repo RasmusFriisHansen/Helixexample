83fbcd923518cbd161f20446322d3deff2b63842
Rasmus Friis Hansen <rfh@alpha-solutions.dk>
2017-10-02 15:49:41 +0200

    Added first testview Tutorial: https://www.youtube.com/watch?v=n2X3P_PZTh0 Finished tutorial.

---
+++ Feature/Blog/code/Controllers/ArticleController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Feature.Blog.Controllers
+{
+  public class ArticleController : Controller
+  {
+    public ArticleController()
+    {
+      
+    }
+
+    public ActionResult List()
+    {
+      return View();
+    }
+
+    public ActionResult Display()
+    {
+      return View();
+    }
+  }
+}
